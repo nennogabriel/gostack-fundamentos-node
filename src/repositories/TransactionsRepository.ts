035acbeb3efb799af01ae4c07559b69994178644
Pedro Moreno <nennogabriel@gmail.com>
2020-07-12 19:21:39 -0300

    Desafio 05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface TransactionsRepositoryDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .map(transaction =>
+        transaction.type === 'income' ? transaction.value : 0,
+      )
+      .reduce((acc, value) => acc + value);
+    const outcome = this.transactions
+      .map(transaction =>
+        transaction.type === 'outcome' ? transaction.value : 0,
+      )
+      .reduce((acc, value) => acc + value);
+    return {
+      income,
+      outcome,
+      total: income - outcome,
+    };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({
+    title,
+    value,
+    type,
+  }: TransactionsRepositoryDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
